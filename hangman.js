ada3ba00216a5ec414dee85763b08107e1429d4d
vievsonik <118842811+vievsonik@users.noreply.github.com>
2023-02-02 19:20:40 +0400

    changes

--- hangman.js
+++ hangman.js
@@ -1,7 +1,12 @@
 const playbutton = document.getElementById ('playbutton');
 const firstDivs = document.getElementById('first-div');
 const mains = document.getElementsByClassName('main')[0];
+const RandomName = document.getElementById('Random-name');
+const randomArr = ['white', 'nike', 'kvara', 'Popcorn'];
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+let gueessTheWord = '';
+let hiddenWord = '';
+
 
 
 
@@ -10,6 +15,8 @@ function generateButtons (letter) {
     btn.innerText = letter;
     btn.addEventListener ('click', () => {
         btn.disabled =true
+        btn.style.backgroundColor = 'red';
+        btn.style.opacity = '50%';
     })
     firstDivs.appendChild(btn);
 }
@@ -18,13 +25,23 @@ function displayButtons () {
         generateButtons(letter)
     })
 }
+function showRandomWord () {
+    gueessTheWord = randomArr[Math.floor(Math.random() * randomArr.length)];
+    for ( let letter of gueessTheWord){
+        hiddenWord += '-';
+    }
+    RandomName.innerText = hiddenWord;
+
+}
 
 
 function play () {
     mains.style.display = 'flex';
-    playbutton.style.display = 'none';
+    playbutton.style.display = 'none'; 
     generateButtons();
     displayButtons();
+    showRandomWord();
+    
 }
 
 
